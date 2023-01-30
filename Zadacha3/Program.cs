983d5608b787792061a7a8dcae9b1ead0ee1236b
Gromov Igor <gaz.oil.gromov@gmail.com>
2023-01-30 20:03:47 +0500

    Zadacha3

--- Zadacha3/Program.cs
+++ Zadacha3/Program.cs
@@ -10,6 +10,7 @@ for (int i = 0; i  <randomArray.Length; i++)
     randomArray[i] = new Random().Next(1,9);
     Console.Write(randomArray[i] + " ");
 }
+
 int ReadInt(string message)
 {
     Console.Write(message);
